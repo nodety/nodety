b35506fc572e555288029aae6d6775105eec1f23
Timo Lehnertz <timo.lehnertz@nerou.de>
2026-04-13 14:16:30 +0200

    add test

---
+++ tests/normalization.rs
@@ -0,0 +1,16 @@
+use crate::common::expr;
+use nodety::{
+    demo_type::DemoType,
+    scope::{Scope, ScopePointer},
+};
+mod common;
+
+#[test]
+fn test_normalization_of_index_and_type_param() {
+    let scope = Scope::<DemoType>::try_parse("<T>").unwrap();
+    scope.infer(&"T".into(), expr("{a: Integer}"), ScopePointer::new_root()).unwrap();
+
+    let normalized = expr("T['a']").normalize(&ScopePointer::new(scope));
+
+    assert_eq!(normalized, expr("Integer"));
+}
